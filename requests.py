1b69ebad64792a5ec50eae55d7d8f4c67fb42566
tz1006 <a@omg.tf>
2017-12-16 20:10:21 -0800

    Create requests.py

---
+++ requests.py
@@ -0,0 +1,20 @@
+#!
+import requests
+
+ua_d = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/602.2.14 (KHTML, like Gecko) Version/10.0.1 Safari/602.2.14'
+ua_m = 'Mozilla/5.0 (iPhone; CPU iPhone OS 11_1_1 like Mac OS X) AppleWebKit/604.3.5 (KHTML, like Gecko) Version/11.0 Mobile/15B150 Safari/604.1'
+header = {'User-Agent':ua_d}
+
+ua = 
+print('''
+-----Desktop-----
+Default
+-----Mobile-----
+header = {\'User-Agent\':ua_m}
+-----Command-----
+url = 
+a = requests.get(url, headers = header, allow_redirects=False).content
+''')
+
+import code
+code.interact(banner = "", local = locals())
